04b9e8f9c1d2cc73552d07a3268ed832200dfdb7
MR_KALIT09 <75316440+MRKALIT09@users.noreply.github.com>
2021-01-08 13:24:20 +0700

    Add files via upload

---
+++ mando-X12.sh
@@ -0,0 +1,377 @@
+#!bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+echo
+echo
+sleep 2
+figlet "GABUD"
+sleep 2
+figlet "ONLINE "
+echo "UNSURNAME : MR"
+echo "PASSWORD  : KALIT09"
+
+echo -n "MASUKAN UNSURNAME SCRIP ==>"
+read t
+echo -n "MASUKAN PASSWORD SCRIP  ==>"
+read b
+echo "LOADING CUK ......."
+sleep 3
+echo "UNSURNAME DAN PASSWORD BENAR"
+sleep 5
+echo -n "TEKAN ENTER UNTUK MASUK SC"
+read b
+echo
+sleep 2
+echo "$cy █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"
+sleep 2
+echo "$cy        ★  AUTHOR : MR.KALIT09  ★"
+sleep 2
+echo "$cy        ★  WA ME :082277804293  ★"
+sleep 2
+echo "$cy    ★  https://github.com/MRKALIT09  ★"
+sleep 2
+echo "$cy      CYBER MEDAN - YUKI CYBER SQUAD "
+sleep 2
+echo "$cy █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+echo
+sleep 2
+echo $i "■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█"
+sleep 2
+echo $ku     "MASUKAN MENU TOOLS GABUD NYA"
+sleep 1
+echo $ku" 1 => HACK PESAWAT"
+sleep 1
+echo $ku" 2 => HACK ORGAN TUBUH"
+sleep 1
+echo $ku" 3 => HACK RUMAH TANGGA ORANG"
+sleep 1
+echo $ku" 4 => HACK HATI MU BUAT KU ><"
+sleep 1
+echo $ku" 5 => EXIT TOOLS YG TOLOL SEDUNIA INI"
+sleep 2
+echo $i "■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█■█"
+echo
+read -p  " ━━━━❰PILIH❱ " pill
+#batas gan
+if [ $pill = "1" ]
+then
+sleep 1
+echo -n $cy "MASUKAN NAMA PESAWAT NYA : "
+read t
+sleep 1
+echo -n $cy "BAGIAN APA YG MAU DI HACK : "
+read b
+sleep 1
+echo -n $cy "APAKAH ANDA YAKIN? NTAR DI PENJARA : "
+read s
+sleep 1
+echo -n $cy "KLICK ENTER JIKA MAU DI PENJARA : "
+read e
+clear
+echo $i " Pesawat Dengan Nama $t Sedang Di Hack"
+echo
+sleep 6
+echo $i " Pesawat Dengan Nama $t Sudah di Hack"
+echo
+sleep 5
+echo $cy " Dan Anda Telah Siap Di DI PENJARA"
+echo
+sleep 5
+echo $cy "PESAWAT DENGAN NAMA $t Hilang"
+echo
+sleep 5
+echo "    ANDA HARUS BERTANGGUNG JAWAB"
+sleep 4
+echo "     DENGAN CARA DI PENJARA:)"
+echo
+sleep 5
+echo "     ADA SUATU PEPATAH"
+echo
+sleep 6
+echo       "APA YG KAU TABUR"
+echo
+sleep 7
+echo       "ITU LAH YG AKAN KAU TUAI"
+echo
+sleep 3
+figlet "Makanya"
+sleep 3
+figlet "Tobat"
+sleep 3
+figlet "Kontol"
+exit
+fi
+#batas gan
+if [ $pill = "2" ]
+then
+sleep 2
+echo -n $cy "MASUKAN NAMA ORANG NYA GAN : "
+read t
+sleep 2
+echo -n  $cy "BAGIAN MANA YG MAU DI HACK : "
+read b
+sleep 2
+echo -n $cy "DOSA NYA KE KAU BUKAN KE AKU YA TOD : "
+read s
+sleep 2
+echo -n $cy "KLICK ENTER KALAU MAU MASUK NERAKA : "
+read e
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+sleep 5
+echo              "Sabar Proses Perusakan Organ Nya Si $t"
+echo  $cy "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo            "MANTAP TOL KITA DAH HACK $b Nya "
+echo  $cy "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo               "AOWKWOOKAOAKA KESAKITAN $b NYA"
+echo  $cy "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo                 "Njir Si $t Dah Mau Otw Surga aoakwowko"
+echo  $cy "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo                "UDAH GAS AJA AMPE MATI :v"
+echo  $cy "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo               "KARNA JADI BAIK ITU TERUS DI SALAHKAN"
+echo  $cy "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo                   "SADBOY AKU NYA TOD XIXIXIXIX"
+echo  $cy "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo               "TOD SI $t DAH MASUK SURGA AOWKWOWKO"
+echo $cy  "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo                    "INI SAAT YG BAGUS TOD"
+echo $cy  "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo                 "KAU NGELAYAT SANA NANTI DAPAT MAKAN GRATIS"
+echo $cy  "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo                 "LUMAYAN TOD MAKAN GRATIS AUTO NAMBAH TERUS"
+echo $cy  "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo                  "YAUDAH LAH TOD MISI NYA SELESAI"
+echo $cy  "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo                "GOD BYE JANGAN LUPA KALAU DAH SIAP NGELAYAT"
+echo $cy  "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+echo                 "BUNGKUS AYAM NYA YA AWOKAWOKAWOK"
+echo $cy  "  █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+sleep 5
+figlet       "GOD BYE"
+sleep 5
+figlet       "MAMPIR"
+sleep 5
+figlet       "Lagi Ya"
+exit
+fi
+#batas gan
+if [ $pill = "3" ]
+then
+echo -n  $i "MASUKAN NAMA ISTRI/SUAMI : "
+read t
+echo -n  $i "SIAPA KAH YG DLUAN MEMULAI SELINGKUH : "
+read b
+echo -n  $i "DOSA TIDAK DI TANGGUNG AUTHOR YA TOL : "
+read s
+echo -n  $i "LANJUT DENGAN MENGKLIK ENTER : "
+read e
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+sleep 8
+echo $i "Si $t=>  NGECEK WHATSAAP SI $b "
+echo
+echo
+sleep 7
+echo
+echo $i "$t=>  TADI AKU LIHAT PESAN WHATSAAP ²"
+sleep 6
+echo  $i "$t=>  KAMU KNAPA MANGGIL DIA SAYANG"
+echo
+echo
+echo
+sleep 6
+echo $i "$b=>  MANA ADA KAMU JANGAN ASAL NUDU JINK"
+echo
+echo
+sleep 6
+echo $i "$t=>  MANA ADA APA NYA SINI AKU LIHAT WA KAMU"
+echo
+echo
+echo
+sleep 6
+echo $i "$b=> Gausah Sentuh² Hp Ku Bangsad"
+echo
+echo
+echo
+sleep 6
+echo $i "$t=> LAH Kok NgegAs Anjink"
+echo
+echo
+echo
+sleep 7
+echo $i "$b=>  IYA LAH NAMA KAU FITNAH ANJG"
+echo
+echo
+echo
+sleep 6
+echo $i "$t=>  TERNYATA AKU SALAH MILIH KEKASIH"
+echo
+echo
+echo
+sleep 6
+echo $i "$b=> YA MEMANG TOLOL AKU INI MATRE"
+echo
+echo
+echo
+sleep 6
+echo $i "$t=> YAUDAH TANPA PIKIR PANJANG KITA PUTUS"
+echo
+echo
+echo
+echo
+sleep 7
+echo $i "$b=> YAUDAH BAGUS LAH AKU PUN DAH BOSAN SAMA KAMU"
+echo
+echo
+echo
+sleep 7
+echo " 5 BULAN KEMUDIAN......."
+sleep 3
+echo $i "$b=> SAYANG AKU MINTA MAFF YA AKU KHILAF"
+echo
+echo
+echo $i "DASAR MANUSIA GDK AHKLAK"
+echo
+echo
+sleep 7
+echo "PADAHAL MREKA PUTUS KARNA KITA YG HACK WA NYA"
+sleep 8
+echo "DAN NGECHAT ORANG DENGAN KATA² MANIS KITA"
+echo
+echo
+echo
+echo "SEKIAN DLU HACK RUMAH TANGGA ORANG"
+sleep 5
+figlet "SEE YOU"
+sleep 5
+figlet "NEXT"
+sleep 5
+figlet "TIME BRO"
+exit
+fi
+#batas gan
+if [ $pill = "4" ]
+then
+sleep 5
+figlet "BUAT LU"
+sleep 5
+figlet "Yg Suka"
+sleep 5
+figlet "Diam² "
+sleep 5
+figlet "SATU KATA"
+sleep 5
+figlet "BUAT LU"
+sleep 5
+figlet "KONTOL"
+sleep 5
+figlet "Sadar Tol"
+sleep 5
+figlet "KITA ORANG"
+sleep 5
+figlet "MISKIN"
+sleep 5
+figlet "JELEK"
+sleep 5
+figlet "LIHAT DIA"
+sleep 5
+figlet "CANTIK"
+sleep 5
+figlet "KAYA"
+sleep 5
+figlet "Sadar Ya"
+sleep 5
+figlet "Dia Ga "
+sleep 5
+figlet "Bakal.Mau"
+sleep 5
+figlet "Cinta Ga"
+sleep 5
+figlet "Bisa Di"
+sleep 5
+figlet "Dipaksa"
+exit
+fi
+#batas gan
+if [ $pill = "5" ]
+then
+sleep 7
+echo "JIKA ADA KESALAHAN DI SCRIP "
+sleep 7
+echo "INGAT NOMOR WA SAYA DI ATAS YA"
+sleep 7
+echo "SAMPAI SINI KITA BERPISAH YA:)"
+sleep 7
+echo " JIKA ADA KESALAHAN MAFF KAN SAYA"
+sleep 7
+echo "JANGAN LUPA BALIK KE SCRIP INI LAGI YA"
+sleep 10
+echo "BERHASIL LOG OUT ........"
+exit
+fi
